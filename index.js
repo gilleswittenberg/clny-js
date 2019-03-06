0478ef7a2dfb303c57650c42c8c3da8688cd4572
Gilles Wittenberg <git@gilleswittenberg.com>
2019-03-06 12:45:03 +0100

    Colony to-json / run

---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require("fs")
+const assert = require("assert").strict
+
+const scripts = ["json", "run"]
+const scriptsString = scripts.join(" / ")
+const message = "Fail!, documentation: `node index.js (" + scriptsString + ") --file`"
+
+const script = process.argv[2]
+assert.ok(scripts.includes(script), message)
+
+assert.ok(process.argv[3], message)
+const path = process.argv[3]
+
+// @TODO: Error handling
+const fileContent = fs.readFileSync(path).toString()
+
+
+// evaluation
+
+const { parse, toPromise } = require("arcsecond")
+const root = require("./parsers/root")
+const util = require("util")
+
+const asData = script === scripts[0] ? true : false
+const evaluate = rootScope => rootScope.evaluate(asData)
+const output = result => console.info(util.inspect(result, { showHidden: false, depth: null, colors: true }))
+
+const onSuccess = rootScope => output(evaluate(rootScope))
+const onError = err => console.error(err)
+
+toPromise(parse(root)(fileContent))
+  .then(onSuccess)
+  .catch(onError)

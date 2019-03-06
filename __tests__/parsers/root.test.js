b4ec7367c32a1d6e5d6383f356fbb2da354c8acf
Gilles Wittenberg <git@gilleswittenberg.com>
2019-03-06 11:33:31 +0100

    Test

--- __tests__/parsers/root.test.js
+++ __tests__/parsers/root.test.js
@@ -20,20 +20,36 @@ test("root scope", () => {
   expect(result.expressions[0].expressions[0]).toBeInstanceOf(Number)
 })
 
-test("deep", () => {
-  const content = `scope:
+describe("deep", () => {
+
+  test("assignments", () => {
+    const content = `scope:
   k:
     a: 9
   l: 6
 my: 9
 `
-  const result = toValue(parse(root)(content))
-  expect(result.expressions.length).toBe(2)
-  expect(result.expressions[0]).toBeInstanceOf(Scope)
-  expect(result.expressions[0].keys).toEqual(["scope"])
-  expect(result.expressions[0].expressions[0]).toBeInstanceOf(Scope)
-  expect(result.expressions[0].expressions[0].keys).toEqual(["k"])
-  expect(result.expressions[1]).toBeInstanceOf(Assignment)
+    const result = toValue(parse(root)(content))
+    expect(result.expressions.length).toBe(2)
+    expect(result.expressions[0]).toBeInstanceOf(Scope)
+    expect(result.expressions[0].keys).toEqual(["scope"])
+    expect(result.expressions[0].expressions[0]).toBeInstanceOf(Scope)
+    expect(result.expressions[0].expressions[0].keys).toEqual(["k"])
+    expect(result.expressions[1]).toBeInstanceOf(Assignment)
+  })
+
+  test("array", () => {
+    const content = `arr:
+  "a"
+  "b"
+`
+    const result = toValue(parse(root)(content))
+    expect(result.expressions.length).toBe(1)
+    expect(result.expressions[0]).toBeInstanceOf(Scope)
+    expect(result.expressions[0].keys).toEqual(["arr"])
+    expect(result.expressions[0].expressions[0]).toBeInstanceOf(String)
+    expect(result.expressions[0].expressions[1]).toBeInstanceOf(String)
+  })
 })
 
 describe("indention", () => {

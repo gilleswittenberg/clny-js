60fdc84c5aaa330ec643f3afe38ad5d1400599a9
Gilles Wittenberg <git@gilleswittenberg.com>
2019-03-09 19:02:16 +0100

    Tests parse conditional statement

--- __tests__/parsers/expressions/expressionsIf.test.js
+++ __tests__/parsers/expressions/expressionsIf.test.js
@@ -15,3 +15,24 @@ test("if statement", () => {
   expect(value.expressions[0].expressions[0]).toBeInstanceOf(Identity)
   expect(value.expressions[0].expressions[1]).toBeInstanceOf(Identity)
 })
+
+test("elseif statement", () => {
+
+  const value = toValue(parse(parser)("elseif b, scope"))
+  expect(value).toBeInstanceOf(Statement)
+  expect(value.name).toBe("elseif")
+  expect(value.expressions[0].expressions.length).toBe(2)
+  expect(value.expressions[0].expressions[0]).toBeInstanceOf(Identity)
+  expect(value.expressions[0].expressions[1]).toBeInstanceOf(Identity)
+})
+
+test("else statement", () => {
+
+  const value = toValue(parse(parser)("else scope"))
+  expect(value).toBeInstanceOf(Statement)
+  expect(value.name).toBe("else")
+  expect(value.expressions.length).toBe(1)
+  expect(value.expressions[0]).toBeInstanceOf(Identity)
+})
+
+// @TODO: Multiple conditionals eg. (if b, scope elseif bb, scopee else bbb, scopeee)
